6cbee49d5c0eba6f1cec4d72b20efac9676b479b
sugawani <nekorollykk@gmail.com>
2020-07-25 23:26:58 +0900

    お試しでapi gateway経由でlambda呼ぶ関数実装

--- Web/script.js
+++ Web/script.js
@@ -1,3 +1,9 @@
 let func = () => {
     window.alert('hello world');
 }
+
+let addition = async () => {
+    const url = 'https://n47n91mso0.execute-api.ap-northeast-1.amazonaws.com/default/aws-toolkit-lambda-test';
+    let result = await fetch(url);
+    console.log(result.json());
+}
